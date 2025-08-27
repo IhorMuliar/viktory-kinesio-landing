fb7a49d266ae0ee5bfa547d0dc91fe23711b79cb
IhorMuliar <goga440@gmail.com>
2025-08-27 09:53:21 +0300

    fix: Update vibrations text

--- app/vibrations/page.jsx
+++ app/vibrations/page.jsx
@@ -52,7 +52,7 @@ const EmotionBalance = () => {
             <h1 className={styles.hero_title}>Як підняти рівень енергії та знайти баланс</h1>
           </div>
           <p className={styles.hero_price}>
-            Лише <b>347 грн</b> замість 1290 грн
+            Лише <b>999 грн</b> замість 2500 грн
           </p>
           <div className={styles.hero_image}>
             <div className={styles.hero_image_text}>
@@ -283,8 +283,8 @@ const EmotionBalance = () => {
             <h2 className={styles.title}>Зроби перший крок до змін</h2>
             <p className={styles.subtext}>
               Запишись на майстер-клас всього за <br />
-              <b>347 грн</b> <br /> замість 1290 грн! <br />
-              Придбай зараз і отримай у подарунок відео техніку «Баланс емоцій»!
+              <b>999 грн</b> <br /> замість 2500 грн! <br />
+              Перші 5 отримають 30 хв зідзвон зі мною для відпрацювання методу!
               <b>Кількість місць обмежена!</b>
             </p>
             <p className={styles.action}>:: Приєднатися до курсу ::</p>
